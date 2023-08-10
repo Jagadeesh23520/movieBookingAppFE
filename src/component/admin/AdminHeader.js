b6c514579a7d506eea624dc3357bc91f7fcb098f
Jagadeesh23520 <jaga23520@gmail.com>
2023-08-10 16:42:26 +0530

    updated front end

---
+++ src/component/admin/AdminHeader.js
@@ -0,0 +1,26 @@
+import {React, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+// import '../../styles/Header.css'
+import './AdminHeader.css';
+
+function AdminHeader(){
+    const navigate = useNavigate();
+    return(
+        <>
+       
+        <nav className='adminHeader'>
+            <button onClick={(event) => {
+                navigate("/home")
+            }}>Home</button>
+
+
+            <button className='buttonlog' onClick={(event) => {
+                navigate("/login")
+            }}>Logout</button>
+        </nav>
+    
+        </>
+    );
+};
+
+export default AdminHeader;
